6dc7f664eaccad66565072eec1ac3761d9dae23d
kikychn <kikychn@gmail.com>
2019-04-13 19:42:15 +0800

    achieve the function of printReminderMessage() for pringting the reminder message

--- src/main/java/com/tw/MenuCommand.java
+++ src/main/java/com/tw/MenuCommand.java
@@ -5,11 +5,15 @@ import java.util.HashMap;
 
 public class MenuCommand {
     public static HashMap<String, String> mainMenu = new HashMap<>();
+    public static HashMap<String, String> reminderMessage = new HashMap<>();
 
     {
         mainMenu.put("1", "添加学生");
         mainMenu.put("2", "生成成绩单");
         mainMenu.put("3", "退出请输入你的选择（1～3）：");
+        reminderMessage.put("1", "请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：");
+        reminderMessage.put("2", "请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
+        reminderMessage.put("3", "");
     }
 
     public String printMainMenu() {
@@ -18,5 +22,9 @@ public class MenuCommand {
             commandsStr.add(commandId + ". " + mainMenu.get(commandId));
         }
         return String.join("\n", commandsStr);
+    }
+
+    public String printReminderMessage(String commandId){
+        return reminderMessage.get(commandId);
     }
 }
--- src/test/java/com/tw/MenuCommandTest.java
+++ src/test/java/com/tw/MenuCommandTest.java
@@ -1,14 +1,26 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MenuCommandTest {
+    MenuCommand mainManu;
+
+    @Before
+    public void setUp() throws Exception {
+        mainManu = new MenuCommand();
+    }
+
     @Test
     public void should_menuCommand_print_mainMenu() {
-        MenuCommand mainManu = new MenuCommand();
         String expexted = "1. 添加学生\n2. 生成成绩单\n3. 退出请输入你的选择（1～3）：";
         assertEquals(expexted, mainManu.printMainMenu());
+    }
+
+    @Test
+    public void should_print_reminderMessage_after_first_input_commandId() {
+        assertEquals("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：", mainManu.printReminderMessage("2"));
     }
 }
