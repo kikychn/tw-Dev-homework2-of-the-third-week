f8d87fa8d22ca97e17bc12d572fccee53ad82525
kikychn <kikychn@gmail.com>
2019-04-13 11:58:22 +0800

    initian the class of Discipline

--- src/test/java/com/tw/StudentTest.java
+++ src/test/java/com/tw/StudentTest.java
@@ -12,4 +12,10 @@ public class StudentTest {
         assertEquals("Tom",student.getName());
     }
 
+    @Test
+    public void should_discipline_have_id_name() {
+        Discipline discipline = new Discipline(1, "数学");
+        assertEquals(1, discipline.getId());
+        assertEquals("数学",discipline.getName());
+    }
 }
