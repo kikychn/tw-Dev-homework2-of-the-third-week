6fcfab5e4b1cab2165196992e2df6019e37ac11d
kikychn <kikychn@gmail.com>
2019-04-13 17:59:44 +0800

    achieve the function of introduce for printing single student's score

--- src/main/java/com/tw/Student.java
+++ src/main/java/com/tw/Student.java
@@ -1,12 +1,50 @@
 package com.tw;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Student {
     private int id;
     private String name;
+    private HashMap<String, Double> allDisciplineScore;
+    private Double averageScore;
+    private Double totalScore;
+    public List<String> disciplines = Arrays.asList("数学", "语文", "英语", "编程");
 
-    public Student(int id, String name) {
+
+    public Student(int id, String name, HashMap allDisciplineScore) {
         this.id = id;
         this.name = name;
+        this.allDisciplineScore = allDisciplineScore;
+        setTotalScore();
+        setAverageScore();
+    }
+
+    public Double getTotalScore() {
+        return totalScore;
+    }
+
+    private void setTotalScore() {
+        this.totalScore = disciplines.stream()
+                .mapToDouble(discipline -> allDisciplineScore.get(discipline)).sum();
+    }
+
+    public Double getAverageScore() {
+        return averageScore;
+    }
+
+    private void setAverageScore() {
+        this.averageScore = totalScore / disciplines.size();
+    }
+
+    public HashMap getAllDisciplineScore() {
+        return allDisciplineScore;
+    }
+
+    public void setAllDisciplineScore(HashMap allDisciplineScore) {
+        this.allDisciplineScore = allDisciplineScore;
     }
 
     public int getId() {
@@ -23,5 +61,16 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String introduce() {
+        String scores = disciplines.stream()
+                .map(discipline -> allDisciplineScore.get(discipline) % 1.0 == 0
+                        ? String.valueOf(allDisciplineScore.get(discipline).longValue())
+                        : String.valueOf(allDisciplineScore.get(discipline)))
+                .collect(Collectors.joining("|"));
+        String averageScoreStr = averageScore % 1.0 == 0 ? String.valueOf(averageScore.longValue()) : String.valueOf(averageScore);
+        String totalScoreStr = totalScore % 1.0 == 0 ? String.valueOf(totalScore.longValue()) : String.valueOf(totalScore);
+        return name + "|" + scores + "|" + averageScoreStr + "|" + totalScoreStr;
     }
 }
--- src/test/java/com/tw/StudentTest.java
+++ src/test/java/com/tw/StudentTest.java
@@ -1,21 +1,48 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.HashMap;
 
 import static org.junit.Assert.assertEquals;
 
 public class StudentTest {
-    @Test
-    public void should_student_have_id_name() {
-        Student student = new Student(1, "Tom");
-        assertEquals(1, student.getId());
-        assertEquals("Tom",student.getName());
+    private Student student;
+
+    @Before
+    public void setUp() throws Exception {
+        HashMap<String, Double> score = new HashMap();
+        score.put("数学", 75.0);
+        score.put("语文", 95.0);
+        score.put("英语", 80.0);
+        score.put("编程", 80.0);
+        student = new Student(1, "张三", score);
     }
 
     @Test
-    public void should_discipline_have_id_name() {
-        Discipline discipline = new Discipline(1, "数学");
-        assertEquals(1, discipline.getId());
-        assertEquals("数学",discipline.getName());
+    public void should_student_have_id_name() {
+        assertEquals(1, student.getId());
+        assertEquals("张三", student.getName());
+        assertEquals(75.0, student.getAllDisciplineScore().get("数学"));
     }
+
+    @Test
+    public void should_student_have_totalScore() {
+        Double expected = 330.0;
+        assertEquals(expected, student.getTotalScore());
+    }
+
+    @Test
+    public void should_student_have_averageScore() {
+        Double expected = 82.5;
+        assertEquals(expected, student.getAverageScore());
+    }
+
+    @Test
+    public void should_student_have_an_introduce_method() {
+        String expected = "张三|75|95|80|80|82.5|330";
+        assertEquals(expected, student.introduce());
+    }
+
 }
